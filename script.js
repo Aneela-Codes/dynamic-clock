873452e53b8215946735cc39961c4fc3db539ceb
Aneela-Codes <63118445+Aneela-Codes@users.noreply.github.com>
2020-10-26 15:01:57 +0500

    ads

--- script.js
+++ script.js
@@ -4,21 +4,7 @@
     const MinuteHand = document.querySelector('[data-minute-hand]');
     const SecondHand = document.querySelector('[data-second-hand]');
 
-    function setClock(){
-        const currentDate= new Date()
-        // console.log(currentDate)
-        const SecondRatio = currentDate.getSeconds()/60
-        // console.log(secondRatio);
-        const MinuteRatio = (SecondRatio + currentDate.getMinutes())/60;
-        // console.log(MinuteRatio);
-        const HourRatio = (MinuteRatio + currentDate.getHours())/12;
-        
-
-        // .............Calling the setRotation Function............
-        setRotation(SecondHand, SecondRatio);
-        setRotation(MinuteHand, MinuteRatio);
-        setRotation(HourHand, HourRatio);
-    }
+  //Here the function comes
 
     function setRotation(element, rotationRatio){
         element.style.setProperty("--rotation", rotationRatio * 360)
